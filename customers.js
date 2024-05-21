8fe1c053a63ca31e70139d2c3c42fa1cdb3c2f29
conor6387 <conor6387@gmail.com>
2024-05-21 15:10:45 +0800

    Completed customer dialog

--- customers.js
+++ customers.js
@@ -70,18 +70,33 @@ function createCustomerDivs(customersData, equipmentFilter = null, nameFilter = 
 		}
 
 		const customerList = document.createElement("ul");
+		customerList.id = "customerList";
 
 		const customerName = document.createTextNode("Name: " + customer.firstName + " " + customer.lastName);
 		const customerMemberStatus = document.createTextNode("Loyalty member: " + (customer.loyaltyMember ? "Yes" : "No"));
 		const customerPurchaseCount = document.createTextNode("Number of purchases: " + customer.purchases.length);
 
 		const customerNameListItem = document.createElement("li");
+		customerNameListItem.id = "customerListItem";
 		customerNameListItem.appendChild(customerName);
 
+		const customerDialog = document.getElementById("customerDialog");
+		customerNameListItem.addEventListener("click", () => {
+			loadCustomerDialogData(customer.id);
+			customerDialog.showModal();
+		});
+
+		const closeButton = document.getElementById("dialogCloseButton");
+		closeButton.addEventListener("click", () => {
+			customerDialog.close();
+		});
+
 		const customerMemberStatusListItem = document.createElement("li");
+		customerMemberStatusListItem.id = "customerListItem";
 		customerMemberStatusListItem.appendChild(customerMemberStatus);
 
 		const customerPurchaseCountListItem = document.createElement("li");
+		customerPurchaseCountListItem.id = "customerListItem";
 		customerPurchaseCountListItem.appendChild(customerPurchaseCount);
 
 		customerList.appendChild(customerNameListItem);
@@ -90,6 +105,84 @@ function createCustomerDivs(customersData, equipmentFilter = null, nameFilter = 
 
 		customersInfoDiv.appendChild(customerList);
 	}
+}
+
+function clearCustomerDialog() 
+{
+	const customersDialogDiv = document.getElementById("customerDialogInfoDiv");
+	while (customersDialogDiv.firstChild) {
+		customersDialogDiv.removeChild(customersDialogDiv.firstChild);
+	}
+}
+
+async function loadCustomerDialogData(Id) 
+{
+	clearCustomerDialog();
+
+	const customersData = await retrieveCustomerData();
+
+	const customer = customersData.customers.find((customer) => customer.id == Id);
+
+	const customerDialogInfoDiv = document.getElementById("customerDialogInfoDiv");
+
+	const customerId = document.createTextNode("Id: " + Id);
+	const customerFirstName = document.createTextNode("First name: " + customer.firstName);
+	const customerLastName = document.createTextNode("Last name: " + customer.lastName);
+	const customerEmail = document.createTextNode("Email: " + customer.email);
+	const customerMemberStatus = document.createTextNode("Loyalty member: " + (customer.loyaltyMember ? "Yes" : "No"));
+
+	const customerIdElement = document.createElement("p");
+	customerIdElement.appendChild(customerId);
+
+	const customerFirstNameElement = document.createElement("p");
+	customerFirstNameElement.appendChild(customerFirstName);
+
+	const customerLastNameElement = document.createElement("p");
+	customerLastNameElement.appendChild(customerLastName);
+
+	const customerEmailElement = document.createElement("p");
+	customerEmailElement.appendChild(customerEmail);
+
+	const customerMemberStatusElement = document.createElement("p");
+	customerMemberStatusElement.appendChild(customerMemberStatus);
+
+	customerDialogInfoDiv.appendChild(customerIdElement);
+	customerDialogInfoDiv.appendChild(customerFirstNameElement);
+	customerDialogInfoDiv.appendChild(customerLastNameElement);
+	customerDialogInfoDiv.appendChild(customerEmailElement);
+	customerDialogInfoDiv.appendChild(customerMemberStatusElement);
+
+	const purchaseList = document.createElement("ul");
+	var totalSpent = 0;
+
+	for (const purchase of customer.purchases) 
+	{
+		totalSpent += +purchase.cost;
+
+		const equipmentPurchased = document.createTextNode(purchase.equipment);
+
+		const purchaseListItem = document.createElement("li");
+		purchaseListItem.appendChild(equipmentPurchased);
+
+		purchaseList.appendChild(purchaseListItem);
+	}
+
+	const customerPurchases = document.createTextNode("Purchases:");
+
+	const customerPurchasesElement = document.createElement("p");
+	customerPurchasesElement.appendChild(customerPurchases);
+
+	customerDialogInfoDiv.appendChild(customerPurchasesElement);
+	customerDialogInfoDiv.appendChild(purchaseList);
+
+	totalSpent = totalSpent.toFixed(2);
+
+	const customerSpend = document.createTextNode("Total spent: $" + totalSpent);
+
+	const customerSpendElement = document.createElement("p");
+	customerSpendElement.appendChild(customerSpend);
+
+	customerDialogInfoDiv.appendChild(customerSpendElement);
 }
 
 async function applyFilterSelections()
